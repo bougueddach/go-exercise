3d30dd236029e4785393800820c193b760dfc950
macbook <bougueddach.anass@gmail.com>
2018-10-05 23:47:59 +0100

    add some error handling

--- proxies/UserProxy.go
+++ proxies/UserProxy.go
@@ -18,7 +18,7 @@ func (proxy UserProxy) GetUserProfile(res http.ResponseWriter, req *http.Request
 	param := chi.URLParam(req, "id")
 	id, err := strconv.Atoi(param)
 	if err != nil {
-		panic(err)
+		http.Error(res, "Bad url param", http.StatusBadRequest)
 	}
 	dto := proxy.GetUser(id)
 	json.NewEncoder(res).Encode(vms.UserVM{dto.Id, dto.Name, dto.Email, dto.Avatar})
@@ -28,13 +28,17 @@ func (proxy UserProxy) UpdateUserProfile(res http.ResponseWriter, req *http.Requ
 	param := chi.URLParam(req, "id")
 	id, err := strconv.Atoi(param)
 	if err != nil {
-		panic(err)
+		http.Error(res, "Bad url param", http.StatusBadRequest)
 	}
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
-		panic(err)
+		http.Error(res, "Error converting results to json", http.StatusInternalServerError)
 	}
+
 	var vm vms.UserUpdateVM
-	json.Unmarshal(body, &vm)
+	err = json.Unmarshal(body, &vm)
+	if err != nil {
+		http.Error(res, "Bad request body", http.StatusBadRequest)
+	}
 	proxy.UpdateUser(id, vm.Name, vm.Email, vm.Avatar)
 }
