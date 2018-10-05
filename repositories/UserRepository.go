98b79435e020f49efaa50d0249039aeeb797c460
macbook <bougueddach.anass@gmail.com>
2018-10-05 17:38:54 +0100

    use actual struct id not slice index

--- repositories/UserRepository.go
+++ repositories/UserRepository.go
@@ -3,9 +3,9 @@ package repositories
 import "go-exercise/entities"
 
 var (
-	u1 = entities.User{1, "omar", "bougueddach@gmail.com", "picture"}
-	u2 = entities.User{2, "ayoub", "bougueddach@gmail.com", "picture"}
-	u3 = entities.User{3, "allali", "bougueddach@gmail.com", "picture"}
+	u1    = entities.User{1, "omar", "bougueddach@gmail.com", "picture"}
+	u2    = entities.User{2, "ayoub", "bougueddach@gmail.com", "picture"}
+	u3    = entities.User{3, "allali", "bougueddach@gmail.com", "picture"}
 	users = []entities.User{u1, u2, u3}
 )
 
@@ -13,12 +13,11 @@ type UserRepository struct {
 	UserRepository IUserRepository
 }
 
-
 func (repository *UserRepository) FindById(id int64) (entities.User, error) {
-	//for i := range repository.users{
-	//	if repository.users[i].Id == id {
-	//		return repository.users[i]
-	//	}
-	//}
-	return users[id], nil
+	for i := range users {
+		if users[i].Id == id {
+			return users[i], nil
+		}
+	}
+	return entities.User{}, nil // That's a bad hack, I should let the client know that I couldn't find it
 }
