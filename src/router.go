6c3a76c9a2553f47ced94ad42f2a5de68bed7300
macbook <bougueddach.anass@gmail.com>
2018-10-20 23:12:51 +0100

    abstract handler

--- src/router.go
+++ src/router.go
@@ -2,26 +2,27 @@ package main
 
 import (
 	"github.com/go-chi/chi"
+	"net/http"
 )
 
-type IChiRouter interface {
-	InitRouter() *chi.Mux
+type IRouter interface {
+	InitRouter() http.Handler
 }
 
 type router struct{}
 
-func (router *router) InitRouter() *chi.Mux {
+func (router *router) InitRouter() http.Handler {
 	r := chi.NewRouter()
-	router.InitUserProxyRoutes(r)
+	router.initUserProxyRoutes(r)
 	return r
 }
 
-func (router *router) InitUserProxyRoutes(r *chi.Mux) {
+func (router *router) initUserProxyRoutes(r *chi.Mux) {
 	userProxy := ControllerContainer().InjectUserProxy()
 	r.Get("/user/{id}", userProxy.GetUserProfile)
 	r.Post("/user/{id}", userProxy.UpdateUserProfile)
 }
 
-func Router() IChiRouter {
+func Router() IRouter {
 	return &router{}
 }
